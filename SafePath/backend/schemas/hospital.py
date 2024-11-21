8669ee4356f387e2c031c8b867f636a5bcf07bc3
Daniel Luca <148398056+daniellucadss@users.noreply.github.com>
2024-11-21 13:09:33 -0300

    Deletando versão anterior do protótipo

--- SafePath/backend/schemas/hospital.py
+++
@@ -1,9 +0,0 @@
-# Importação de bibliotecas externas
-from pydantic import BaseModel
-
-# Definição do modelo de dados para um hospital
-class Hospital(BaseModel):
-    id: str            
-    name: str          
-    latitude: float    
-    longitude: float   
